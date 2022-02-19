11290dcbf897a47dd1e4118c3be4e37515ef4a57
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-02-19 10:20:29 +0700

    Create eventOnAddTicket.js

---
+++ TicketsEvent/eventOnAddTicket.js
@@ -0,0 +1,17 @@
+const { Client } require('discord.js').ticketEvents()
+
+// events: Add Ticket //
+/*
+* use event.AddTicketOn('Ticket by ${user.tag.name}') 
+* class.end
+*/
+function AddTicketOn(textchannel){
+  client.addTicket => {
+   name: 'Metadata', // Metadata Events
+   host: 'event.ticketJS.chat', // Host TicketJS
+   ip: 'event.ticketsJS.chat/request/ip' // IP TicketsEvent 
+}
+let client = ReadFile$client.map
+return 'Successfully Created Ticket.map'
+
+}
